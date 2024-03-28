7e4887429d62ebd2319abc4ae4d20f8b79867cd4
CrimsonCrips <danecarmona2@gmail.com>
2024-03-28 22:07:54 +0800

    yes

---
+++ src/client/java/com/crimsoncrips/BEAnvilRepairClient.java
@@ -0,0 +1,10 @@
+package com.crimsoncrips;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BEAnvilRepairClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
